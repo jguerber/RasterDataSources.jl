79581973668af0339ad3611b642e6348a125dbe6
Josquin Guerber <josquin.guerber@gmail.com>
2022-08-01 21:38:47 +0200

    WIP for the generic functions

--- src/modis/shared.jl
+++ src/modis/shared.jl
@@ -47,6 +47,70 @@ end
 
 layers(T::Type{<:ModisProduct}) = layers(MODIS{T})
 
+function get_raster(T::Type{<:ModisProduct}, layer::Union{Tuple, Symbol, Int};
+    lat::Real,
+    lon::Real,
+    km_ab::Int,
+    km_lr::Int,
+    from::Union{String, Date},
+    to::Union{String, Date}
+)
+    _get_raster(T, layer;
+        lat = lat,
+        lon = lon,
+        km_ab = km_ab,
+        km_lr = km_lr,
+        from = from,
+        to = to
+    )
+end
+
+function _get_raster(T::Type{<:ModisProduct}, layer::Symbol;
+    kwargs...
+)
+    _get_raster(T, modis_int(T, layer); kwargs)
+end
+
+function _get_raster(T::Type{<:ModisProduct}, layer::Int;
+    lat::Real,
+    lon::Real,
+    km_ab::Int,
+    km_lr::Int,
+    from::Union{String, Date},
+    to::Union{String, Date}
+)
+    dates = list_dates(T;
+        lat = lat,
+        lon = lon,
+        format = "Date"
+    )
+
+    if length(dates) <= 10
+        _get_raster(T, layer;
+            lat = lat,
+            lon = lon,
+            km_ab = km_ab,
+            km_lr = km_lr,
+            dates = dates
+        )
+    else
+
+    end
+end
+
+function rasterpath(T::Type{<:ModisProduct})
+    return joinpath(rasterpath(), "MODIS", string(nameof(T)))
+end
+
+function rastername(T::Type{<:ModisProduct}, layer::Int; kwargs...)
+    name = "$(layerkeys(T)[layer])_$(kwargs[:lat])_$(kwargs[:lon])_$(kwargs[:date]).tif"
+    return name
+end
+
+function rastername(T::Type{<:ModisProduct}; kwargs...)
+    name = "raster_$(kwargs[:lat])_$(kwargs[:lon])_$(kwargs[:date]).tif"
+    return name
+end
 
 
 
